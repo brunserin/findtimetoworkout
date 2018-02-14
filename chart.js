c0fd2101ebed228d0a87adb27e2a8439759f8e2c
Corinne Burger <corinneburger1@gmail.com>
2018-02-13 18:57:58 -0600

    updating chart stuff

--- chart.js
+++ chart.js
@@ -6,16 +6,16 @@ var stepsRemaining = totalSteps - stepsCompleted;
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
+  var data1 = google.visualization.arrayToDataTable([
     ['Type', 'Number'],
     ['Completed',     stepsCompleted],
     ['Remaining',     stepsRemaining]
   ]);
 
-  var options = {
-      title: "Steps",
+  var options1 = {
+      title: "Steps: 10,000 per day",
       titleTextStyle: { color: "black",
-        fontSize: 40,
+        fontSize: 20,
       },
       pieHole: 0.5,
       slices: {
@@ -25,6 +25,29 @@ function drawChart() {
       legend: {position: 'right', textStyle: {color: 'black', fontSize: 16}}
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  chart.draw(data, options);
+  var data2 = google.visualization.arrayToDataTable([
+    ['Type', 'Number'],
+    ['Completed',     11],
+    ['Remaining',     2]
+  ]);
+
+  var options2 = {
+      title: "Running: 20 miles per week",
+      titleTextStyle: { color: "black",
+        fontSize: 20,
+      },
+      pieHole: 0.5,
+      slices: {
+            0: { color: 'orange' },
+            1: { color: 'red' }
+      },
+      legend: {position: 'right', textStyle: {color: 'black', fontSize: 16}}
+  };
+
+  var chart1 = new google.visualization.PieChart(document.getElementById('donutchart1'));
+  var chart2 = new google.visualization.PieChart(document.getElementById('donutchart2'));
+
+
+  chart1.draw(data1, options1);
+  chart2.draw(data2, options2);
 }
