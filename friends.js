d551a4795791c2d3434d89c465c465221ee0b9fd
Corinne Burger <corinneburger1@gmail.com>
2018-03-08 16:59:00 -0600

    liking

--- friends.js
+++ friends.js
@@ -1,17 +1,28 @@
-function increment1(){
-  console.log("test");
-  return;
+var item1LikeCount = 13;
+var item2LikeCount = 5;
+var liked1 = false;
+var liked2 = false;
+
+function addOne(){
+	if(liked1 == false){
+		liked1 = true;
+		item1LikeCount++;
+		document.getElementById("newsFeedListItem1").innerHTML = "<div class='fas fa-thumbs-up' style='color: blue'></div> " + item1LikeCount;
+	} else {
+		liked1 = false;
+		item1LikeCount--;
+		document.getElementById("newsFeedListItem1").innerHTML = "<div class='fas fa-thumbs-up'></div> " + item1LikeCount;
+	}
 }
 
-var nfItem = document.getElementById("newsFeedListItem1");
-
-
-
-
-window.onclick = function(event) {
-  console.log("Test");
-
-    if (event.target == nfItem) {
-        nfItem.innerHTML = " 14";
-    }
+function addTwo(){
+	if(liked2 == false){
+		liked2 = true;
+		item2LikeCount++;
+		document.getElementById("newsFeedListItem2").innerHTML = "<div class='fas fa-thumbs-up' style='color: blue'></div> " + item2LikeCount;
+	} else {
+		liked2 = false;
+		item2LikeCount--;
+		document.getElementById("newsFeedListItem2").innerHTML = "<div class='fas fa-thumbs-up'></div> " + item2LikeCount;
+	}
 }
