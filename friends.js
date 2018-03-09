7517acefccd9eab350bee733732c151de0f138a7
Corinne Burger <corinneburger1@gmail.com>
2018-03-08 20:08:33 -0600

    confetti wahoo

--- friends.js
+++ friends.js
@@ -7,11 +7,11 @@ function addOne(){
 	if(liked1 == false){
 		liked1 = true;
 		item1LikeCount++;
-		document.getElementById("dCelebrate1").innerHTML = '<img class="celebrate-icon" src="celebrate.png"> ' + item1LikeCount;
+		document.getElementById("dCelebrate1").innerHTML = '<img class="celebrate-icon" src="colorCongrats.png"> ' + item1LikeCount;
 	} else {
 		liked1 = false;
 		item1LikeCount--;
-		document.getElementById("dCelebrate1").innerHTML = '<img class="celebrate-icon" src="celebrateLess.png"> ' + item1LikeCount;
+		document.getElementById("dCelebrate1").innerHTML = '<img class="celebrate-icon" src="greyCongrats.png"> ' + item1LikeCount;
 	}
 }
 
@@ -19,10 +19,10 @@ function addTwo(){
 	if(liked2 == false){
 		liked2 = true;
 		item2LikeCount++;
-		document.getElementById("dCelebrate2").innerHTML = '<img class="celebrate-icon" src="celebrate.png"> ' + item2LikeCount;
+		document.getElementById("dCelebrate2").innerHTML = '<img class="celebrate-icon" src="colorCongrats.png"> ' + item2LikeCount;
 	} else {
 		liked2 = false;
 		item2LikeCount--;
-		document.getElementById("dCelebrate2").innerHTML = '<img class="celebrate-icon" src="celebrateLess.png"> ' + item2LikeCount;
+		document.getElementById("dCelebrate2").innerHTML = '<img class="celebrate-icon" src="greyCongrats.png"> ' + item2LikeCount;
 	}
 }
